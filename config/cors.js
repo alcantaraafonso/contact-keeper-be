6140791012caaedb1960bc5e2be9e68203c5f3f3
Afonso <alcantara.afonso@gmail.com>
2020-06-25 17:40:47 -0300

    CORS

--- config/cors.js
+++ config/cors.js
@@ -5,6 +5,6 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Methods',
         'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token')
     next()
 }
